f83f6d206474e4fc411138295d35693ffd0bf249
francisco lopez <flopez@inovabiz.com>
2021-10-14 14:29:36 -0300

    Primer commit creacion de proyecto Spring boot - Java8.

---
+++ src/main/java/com/nzt/examplerestapi/ExampleRestAPiApplication.java
@@ -0,0 +1,13 @@
+package com.nzt.examplerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleRestAPiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleRestAPiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nzt/examplerestapi/ExampleRestAPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nzt.examplerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleRestAPiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
